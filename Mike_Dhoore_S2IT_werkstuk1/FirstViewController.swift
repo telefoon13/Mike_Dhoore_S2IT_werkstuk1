efef5ebacf55290e2f413f86e0a9dfc9e00573fd
telefoon13 <mike.d.hoore@telenet.be>
2018-04-12 10:00:58 +0200

    Initial Commit

---
+++ Mike_Dhoore_S2IT_werkstuk1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mike_Dhoore_S2IT_werkstuk1
+//
+//  Created by student on 12/04/18.
+//  Copyright © 2018 Mike Dhoore. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mike_Dhoore_S2IT_werkstuk1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mike_Dhoore_S2IT_werkstuk1
+//
+//  Created by student on 12/04/18.
+//  Copyright © 2018 Mike Dhoore. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
